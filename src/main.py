e083e7a3c430ff6d427fb6dc30fdee6478956041
Alexey Rybakov <rybakov.aax@gmail.com>
2021-02-11 14:46:07 +0300

    new grids

--- src/main.py
+++ src/main.py
@@ -14,26 +14,26 @@ if __name__ == '__main__':
     g.print_info()
 
     # Distributions.
-    g.distribute_mono(new_name='GRID mono')
+    g.distribute_mono(new_name=test + ' mono')
     g.print_info()
     g.store('grids/' + test + '_mono.dat')
     #
-    g.distribute_random(new_name='GRID random')
+    g.distribute_random(new_name=test + ' random')
     g.print_info()
     g.store('grids/' + test + '_random.dat')
     #
-    g.distribute_linear(new_name='GRID linear')
+    g.distribute_linear(new_name=test + ' linear')
     g.print_info()
     g.store('grids/' + test + '_linear.dat')
     #
-    g.distribute_rgrow(new_name='GRID rgrow')
+    g.distribute_rgrow(new_name=test + ' rgrow')
     g.print_info()
     g.store('grids/' + test + '_rgrow.dat')
     #
     g.distribute_hierarchical([lambda f: f.center_point()[0],
                                lambda f: f.center_point()[1],
                                lambda f: f.center_point()[2]],
-                              new_name='GRID hierarchical')
+                              new_name=test + ' hierarchical')
     g.print_info()
     g.store('grids/' + test + '_hierarchical.dat')
 
