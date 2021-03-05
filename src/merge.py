40713ee05bb6f9069baab174109e3c95edacc668
Alexey Rybakov <rybakov.aax@gmail.com>
2021-03-05 15:09:11 +0300

    merge : debug

--- src/merge.py
+++ src/merge.py
@@ -19,23 +19,30 @@ def merge_txt_from_mpi(grid_file, txt_files_dir):
     :param txt_files_dir: dir with txt files
     """
 
-    print('crys-gsu : merge_txt_from_mpi : '
-          'grid file = {0}, txt_files_dir = {1}'.format(grid_file, txt_files_dir))
-
     pp = pathlib.PurePath(grid_file)
-    stem = pp.stem
+    # Get characteristics of file:
+    #   bs - base of path
+    #   nm - name without timestamp
+    #   ts - timestamp string
+    #   sf - suffix
+    bs, sf = str(format(pp.parents[0])), pp.suffix
+    nm, ts = utils.get_filename_and_timestamp_pair(pp.stem)
+
+    print('crys-gsu : merge_txt_from_mpi : grid file = '
+          '{0} (bs {1}, nm {2}, ts {3}, sf {4}), '
+          'txt_files_dir = {5}'.format(grid_file, bs, nm, ts, sf, txt_files_dir))
+
     all_files = os.listdir(txt_files_dir)
 
     # We have to analyze only our *.txt files.
-    match_files = [fn for fn in all_files if utils.is_filename_correct_crys_txt_file(fn, stem)]
+    match_files = [fn for fn in all_files if utils.is_filename_correct_crys_txt_file(fn, nm)]
 
     # Add all faces data from cry files.
     g = gsu.Grid()
     g.load(grid_file)
     for fn in match_files:
         g.load_faces_t_hw_hi(txt_files_dir + '/' + fn)
-    nm, ts = utils.get_filename_and_timestamp_pair(stem)
-    g.store('{0}_r_000000000100.dat'.format(stem))
+    g.store('{0}/{1}_r_000000000100.dat'.format(bs, nm))
 
     print('crys-gsu : merge_txt_from_mpi : done (data from {0} files is added)'.format(len(match_files)))
 
