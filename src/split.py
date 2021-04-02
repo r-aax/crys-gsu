cf22a9d96c363a7d3aedb22395fc0810b8b075da
Alexey Rybakov <rybakov.aax@gmail.com>
2021-04-02 14:38:25 +0300

    POS zones extraction

--- src/split.py
+++ src/split.py
@@ -52,7 +52,10 @@ def split(grid_file, cry_dir, split_policy, fixed_zones=[]):
         n = int(split_policy[1:])
         target_zones_count = 2 ** n + len(fixed_zones)
         print('crys-gsu-split : hierarchical, target-zones-count={0} '
-              '(2^{1} + {2})'.format(target_zones_count, n, len(fixed_zones)))
+              '(2^{1} + {2}) // {3}'.format(target_zones_count,
+                                            n,
+                                            len(fixed_zones),
+                                            fixed_zones))
         g.decompose_hierarchical(extract_signs_funs=[gsu.fun_face_cx(), gsu.fun_face_cy(), gsu.fun_face_cz()],
                                  levels=n + 1,
                                  fixed_zones=fixed_zones)
@@ -91,20 +94,42 @@ def print_help():
     print('        if there is no such directory, it will be created')
     print('    <split-strategy>:')
     print('        h<n> - hierarchical split into 2^n zones in addition to AIS zones')
-    print('    <ais-zone-name-i> - names of AIS zones')
+    print('    <json> - json file from which names of ones should be extracted')
     print('')
     print('[Examples]:')
     print('bunny.dat -> bunny_<mpi_i>_000000000000.cry')
     print('bunny_000000000100.dat -> bunny_<mpi_i>_000000000100.cry')
 
+
+# ----------------------------------------------------------------------------------------------------------------------
+
+def extract_fixed_zones_from_json(filename):
+    """
+    Extract fixed zones from json.
+    :param filename: file name
+    :return:
+    """
+
+    f = open(filename, 'r')
+    ll = f.readlines()
+    f.close()
+
+    # Find zones for AIS.
+    ll = [s.split(':')[1] for s in ll if '"zone"' in s]
+    ll = [s[s.index('"') + 1: s.rindex('"')] for s in ll]
+
+    return ll
+
 # ----------------------------------------------------------------------------------------------------------------------
 
 
 # split.py should be called from shell script in the following manner:
-#     split.py grids/bunny.dat grids/cry h2 "POS1" "POS2"
+#     split.py grids/bunny_pos.dat grids/cry h2 grids/bunny.json
 if __name__ == '__main__':
 
-    if len(sys.argv) == 1:
+    c = len(sys.argv)
+
+    if c == 1:
         print_help()
         exit(0)
 
@@ -112,12 +137,18 @@ if __name__ == '__main__':
         print_help()
         exit(0)
 
-    if len(sys.argv) < 4:
+    if c < 4:
         raise Exception('crys-gsu-split : not enough arguments')
+    elif c == 4:
+        fixed_zones = []
+    elif c == 5:
+        fixed_zones = extract_fixed_zones_from_json(sys.argv[4])
+    else:
+        raise Exception('crys-gsu-split : too many arguments')
 
     split(grid_file=sys.argv[1],
           cry_dir=sys.argv[2],
           split_policy=sys.argv[3],
-          fixed_zones=sys.argv[4:])
+          fixed_zones=fixed_zones)
 
 # ----------------------------------------------------------------------------------------------------------------------
