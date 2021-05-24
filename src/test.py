4a07dce0bce0f0e9c234b1d80c50dd30560c43a9
Alexey Rybakov <rybakov.aax@gmail.com>
2021-05-24 13:18:48 +0300

    correct function call

--- src/test.py
+++ src/test.py
@@ -136,7 +136,7 @@ def case_006_store_faces_t_hw_hi(test='bunny'):
     print('case_006_store_faces_t_hw_hi({0}):'.format(test))
     g = gsu.Grid()
     g.load('grids/{0}.dat'.format(test))
-    g.store_faces_t_hw_hi('grids/{0}.txt'.format(test))
+    g.store_faces_calc_data('grids/{0}.txt'.format(test))
 
 # --------------------------------------------------------------------------------------------------
 
@@ -152,8 +152,8 @@ def case_007_load_faces_t_hw_hi(test='bunny'):
     print('case_007_load_faces_t_hw_hi({0}):'.format(test))
     g = gsu.Grid()
     g.load('grids/{0}.dat'.format(test))
-    g.store_faces_t_hw_hi('grids/{0}.txt'.format(test))
-    g.load_faces_t_hw_hi('grids/{0}.txt'.format(test))
+    g.store_faces_calc_data('grids/{0}.txt'.format(test))
+    g.load_faces_calc_data('grids/{0}.txt'.format(test))
     g.store('grids/{0}_data.dat'.format(test))
 
 # --------------------------------------------------------------------------------------------------
