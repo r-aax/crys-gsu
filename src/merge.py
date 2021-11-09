991ddb342ddab12d24e53384199aca838a70623e
Alexey Rybakov <rybakov.aax@gmail.com>
2021-11-10 01:15:49 +0300

    merge : csv

--- src/merge.py
+++ src/merge.py
@@ -8,17 +8,19 @@ import pathlib
 import utils
 import gsu
 import time
+import numpy as np
+from numpy import genfromtxt
 
 # --------------------------------------------------------------------------------------------------
 
 
-def merge(grid_file, txt_files_dir, r_files_dir):
+def merge(grid_file, src_dir, dst_dir):
     """
     Merge txt files with original grid.
     Merge files separately for each timestamp and produce out _r_ file for crys-remesh tool.
     :param grid_file: file with original grid
-    :param txt_files_dir: dir with txt files
-    :param r_files_dir: out dir for _r_ files
+    :param src_dir: directory with sources
+    :param dst_dir: destination directory
     """
 
     start_time = time.time()
@@ -37,10 +39,10 @@ def merge(grid_file, txt_files_dir, r_files_dir):
         raise Exception('crys-gsu-merge : no such file ({0})'.format(grid_file))
 
     print('crys-gsu-merge : grid-file={0} (bs={1}, nm={2}, ts={3}, sf={4}), '
-          'txt_files_dir={5}, r_files_dir={6}'.format(grid_file, bs, nm, ts, sf,
-                                                      txt_files_dir, r_files_dir))
+          'src_dir={5}, dst_dir={6}'.format(grid_file, bs, nm, ts, sf,
+                                            src_dir, dst_dir))
 
-    all_files = os.listdir(txt_files_dir)
+    all_files = os.listdir(src_dir)
 
     # We have to analyze only our *.txt files.
     match_files = [fn for fn in all_files if utils.is_filename_correct_crys_txt_file(fn, nm)]
@@ -50,7 +52,7 @@ def merge(grid_file, txt_files_dir, r_files_dir):
 
     # Make dirs
     try:
-        os.makedirs(r_files_dir)
+        os.makedirs(dst_dir)
     except FileExistsError:
         # Ignore if directories already exist.
         pass
@@ -61,8 +63,21 @@ def merge(grid_file, txt_files_dir, r_files_dir):
         fs = gmf.get(tm)
         g.load(grid_file)
         for f in fs:
-            g.load_faces_calc_data(txt_files_dir + '/' + f)
-        g.store('{0}/{1}_r_{2}.dat'.format(r_files_dir, nm, tm))
+            g.load_faces_calc_data(src_dir + '/' + f)
+        g.store('{0}/{1}_r_{2}.dat'.format(dst_dir, nm, tm))
+
+    # Finally put total *.csv file to dst_dir/../ directory.
+    csv_files = [fn for fn in all_files if fn[-4:] == '.csv']
+    if len(csv_files) > 1:
+        d = genfromtxt(src_dir + '/' + csv_files[0], delimiter=';', skip_header=True)
+        t = d[:, :1]
+        d = d[:, 1:]
+        for csv_file in csv_files[1:]:
+            d = d + genfromtxt(src_dir + '/' + csv_file, delimiter=';', skip_header=True)[:, 1:]
+        d = np.hstack((t, d))
+        np.savetxt('{0}/../{1}.csv'.format(dst_dir, nm), d, delimiter=";",
+                   header='time;M_imp;M_es;M_stall;M_ini_water;M_water;M_ini_ice;M_ice;Q_conv;Q_es',
+                   comments='')
 
     print('crys-gsu-merge : done ({0} _r_ files generated, '
           '{1:.3f} seconds spent)'.format(len(gmf.keys()), time.time() - start_time))
@@ -81,12 +96,12 @@ if __name__ == '__main__':
                                      description='Merge calc data.',
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('grid_file', help='grid file name')
-    parser.add_argument('txt_files_dir', help='txt files with calc data')
-    parser.add_argument('r_files_dir', help='dir with files for remesher')
+    parser.add_argument('src_dir', help='directory with source files')
+    parser.add_argument('dst_dir', help='destination directory')
     args = parser.parse_args()
 
     merge(grid_file=args.grid_file,
-          txt_files_dir=args.txt_files_dir,
-          r_files_dir=args.r_files_dir)
+          src_dir=args.src_dir,
+          dst_dir=args.dst_dir)
 
 # --------------------------------------------------------------------------------------------------
