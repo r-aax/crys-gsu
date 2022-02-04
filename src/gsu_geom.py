9e2c413653ab4ecc3efd29947c624d2c9cbac1cc
freysa <freysa@jscc.ru>
2022-02-04 11:45:10 +0300

    тесты для gsu_geom #25

--- src/gsu_geom.py
+++ src/gsu_geom.py
@@ -791,23 +791,6 @@ if __name__ == '__main__':
     Main function.
     """
 
-    # No intersection by boxes.
-    t1 = Triangle(Vect(0.0, 0.0, 0.0), Vect(1.0, 0.0, 0.0), Vect(0.0, 1.0, 0.0))
-    t2 = Triangle(Vect(6.0, 0.0, 0.0), Vect(5.0, 1.0, 0.0), Vect(5.0, 0.0, 0.0))
-    assert t1.is_no_intersection_with_triangle_by_boxes(t2)
-
-    # Exact equal.
-    t1 = Triangle(Vect(0.0, 0.0, 0.0), Vect(1.0, 0.0, 0.0), Vect(0.0, 1.0, 0.0))
-    t2 = Triangle(Vect(1.0, 0.0, 0.0), Vect(0.0, 1.0, 0.0), Vect(0.0, 0.0, 0.0))
-    assert t1.is_eq(t2)
-
-    # Common edge.
-    t1 = Triangle(Vect(0.0, 0.0, 0.0), Vect(1.0, 0.0, 0.0), Vect(0.0, 1.0, 0.0))
-    t2 = Triangle(Vect(1.0, 0.0, 0.0), Vect(0.0, 1.0, 0.0), Vect(1.0, 1.0, 0.0))
-    assert t1.is_has_common_edge_with_triangle(t2)
-
-    # Area.
-    t = Triangle(Vect(0.0, 0.0, 0.0), Vect(1.0, 0.0, 0.0), Vect(0.0, 1.0, 0.0))
-    assert abs(t.area() - 0.5) < eps
+    pass
 
 # ==================================================================================================
